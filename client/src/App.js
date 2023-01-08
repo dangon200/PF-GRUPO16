05725572bd7fe852fb3a2c3bdf517c42809a3ba6
Agustin Fraile <agustinfraile98@gmail.com>
2023-01-07 22:48:34 -0300

    agregando seccion de equipo

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
+import NavBar from 'components/NavBar';
+import Home from 'pages/Home/Home';
+import SobreNosotros from 'pages/SobreNosotros/SobreNosotros';
+import Tienda from 'pages/Tienda/Tienda';
+import { Switch, Route } from 'react-router-dom';
 
-import NavBar from 'components/NavBar'
-import Home from 'pages/Home/Home'
-import Tienda from 'pages/Tienda/Tienda'
-import { Switch, Route } from 'react-router-dom'
-
-import './App.css'
+import './App.css';
 
 
 function App() {
     return (
         <div className="App">
             
-            <Route exact path={['/', '/tienda']} component={NavBar} />
+            <Route 
+                exact path={['/', '/tienda', '/nosotros']} component={NavBar} 
+            />
             <Switch>
-
-    
                 <Route exact path="/" component={Home} />
                 <Route path="/tienda" component={Tienda} />
+                <Route path="/nosotros" component={SobreNosotros} />
             </Switch>
             
         </div>
     )
 }
 
-export default App
+export default App;
