fb5874fc256d59df69febc38cf203239ec054db7
Agustin Fraile <agustinfraile98@gmail.com>
2023-01-07 22:58:01 -0300

    solucionando la card de comentarios de expertos

--- client/src/components/CardExpertos/CardExpertos.jsx
+++ client/src/components/CardExpertos/CardExpertos.jsx
@@ -5,17 +5,17 @@ import './CardExpertos.css'
 
 const CardExpertos = () => {
   return (
-    <div className='card_container'>
-        <div className='card_container-info'>
-            <div className='card_container-img--cnt'> 
-                <div className='card_container-imagen'></div>
+    <div className='card_expertos_container'>
+        <div className='card_expertos_container-info'>
+            <div className='card_expertos_container-img--cnt'> 
+                <div className='card_expertos_container-imagen'></div>
             </div>
-            <div className='card_container-title--cnt'>
+            <div className='card_expertos_container-title--cnt'>
                 <h4>
                     “Comprar aqui es lo mejor que me paso en la vida”
                 </h4>
             </div>
-            <div className='card_container-text--cnt'>
+            <div className='card_expertos_container-text--cnt'>
                 <p>
                     Elon Musk en la revista Caras 2001
                 </p>
